121da8bd6125ee1f6a8c2b1f7fee286e9b68bd63
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-11-08 19:54:41 +0900

    배포 스크립트 수정

--- deploy/AfterInstall.sh
+++ deploy/AfterInstall.sh
@@ -3,13 +3,22 @@
 # 배포 주소
 BASE_PATH="/home/ubuntu/www"
 
-sudo apt-get update
-
-# docker 설치
-sudo apt install -y docker.io
-
 cd ${BASE_PATH}
 
-sudo docker compose -f docker-compose.prod.yml up -d
+sudo docker run -d \
+  -p 80:80 \
+  --name proxy \
+  qwp1216/blog-server-proxy
+
+sudo docker run -d \
+  -p 6379:6379 \
+  -v ./redis-data:/data \
+  --name redis \
+  redis
+
+sudo docker run -d \
+  -p 3001:3001 \
+  --name web-server \
+  qwp1216/blog-server
 
 sudo docker images --quiet --filter=dangling=true | sudo xargs --no-run-if-empty docker rmi
