439ecf07fc3af21fa1ee484e7f3399787cf0a3b7
qwp0905 <qwp0905@gmail.com>
2023-02-04 19:46:21 +0900

    fix database config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,12 +2,25 @@ import { DataSourceOptions } from 'typeorm'
 
 export const TypeOrmConfig: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: +process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
   entities: ['{dist/**/*.entity.js,src/**/*.entity.ts}'],
   entitySkipConstructor: true,
-  synchronize: false
+  synchronize: true,
+  replication: {
+    master: {
+      host: process.env.DATABASE_HOST_MASTER,
+      port: +process.env.DATABASE_PORT_MASTER,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME
+    },
+    slaves: [
+      {
+        host: process.env.DATABASE_HOST_SLAVE,
+        port: +process.env.DATABASE_PORT_SLAVE,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME
+      }
+    ]
+  }
 }
