e538147945536bc92d6dcfed241b24750b9c78a8
qwp0905 <qwp0905@gmail.com>
2023-02-23 21:55:26 +0900

    add stream object

--- src/shared/lib/request-handler.ts
+++ src/shared/lib/request-handler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { DateFormat } from '../utils/date'
+import { StreamObject } from './stream'
 
 export type Handler = (
   req: Request,
@@ -13,6 +14,38 @@ export const Wrap =
     try {
       const data = await handler(req, res, next)
       res.json({ result: true, data, timestamp: DateFormat() })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+export type StreamHandler = (
+  req: Request,
+  res?: Response,
+  next?: NextFunction
+) => StreamObject<any>
+
+export const WrapStream = (handler: Handler): RequestHandler =>
+  async function (req, res, next) {
+    try {
+      const stream_object = await handler(req)
+
+      const stream = stream_object.getStream()
+
+      res.setHeader('Content-Type', 'application/json')
+
+      stream.on('data', (chunk: any) => {
+        res.write(JSON.stringify(chunk) + '\n')
+      })
+
+      stream.on('error', (err: unknown) => {
+        stream.emit('close')
+        next(err)
+      })
+
+      stream.on('end', () => {
+        res.status(200).end()
+      })
     } catch (error) {
       next(error)
     }
---
+++ src/shared/lib/stream.ts
@@ -0,0 +1,124 @@
+import { Stream, Transform } from 'stream'
+
+type TFilterCallback<T> = (arg: T, index?: number) => boolean | Promise<boolean>
+type TMapCallback<T, R> = (arg: T, index?: number) => R | Promise<R>
+type TTapCallback<T> = (arg: T, index?: number) => void | Promise<void>
+type TReduceCallback<A, C> = (acc: A, cur: C, index?: number) => A | Promise<A>
+
+export class StreamObject<T> {
+  constructor(private stream: Stream) {}
+
+  getStream() {
+    return this.stream
+  }
+
+  toPromise() {
+    return new Promise<T>((res, rej) => {
+      let result: T
+      this.stream.on('data', (chunk: T) => {
+        result = chunk
+      })
+
+      this.stream.on('error', (err) => rej(err))
+
+      this.stream.on('end', () => res(result))
+    })
+  }
+
+  toArray() {
+    return new Promise<T[]>((res, rej) => {
+      const result: T[] = []
+      this.stream.on('data', (chunk: T) => {
+        result.push(chunk)
+      })
+
+      this.stream.on('error', (err) => rej(err))
+
+      this.stream.on('end', () => res(result))
+    })
+  }
+
+  filter(callback: TFilterCallback<T>) {
+    return fromStream<T>(this.stream.pipe(filterStream(callback)))
+  }
+
+  map<R = T>(callback: TMapCallback<T, R>) {
+    return fromStream<R>(this.stream.pipe(mapStream<T, R>(callback)))
+  }
+
+  tap(callback: TTapCallback<T>) {
+    return fromStream<T>(this.stream.pipe(tapStream<T>(callback)))
+  }
+
+  reduce<A = T>(callback: TReduceCallback<A, T>, initialValue: A) {
+    return fromStream<A>(this.stream.pipe(reduceStream<A, T>(callback, initialValue)))
+  }
+}
+
+export const fromStream = <T = any>(stream: Stream) => new StreamObject<T>(stream)
+
+export const filterStream = <T>(callback: TFilterCallback<T>) => {
+  return new Transform({
+    objectMode: true,
+    transform: async (chunk, _, next) => {
+      try {
+        const condition = await callback(chunk)
+        condition ? next(null, chunk) : next()
+      } catch (err) {
+        next(err)
+      }
+    }
+  })
+}
+
+export const tapStream = <T>(callback: TTapCallback<T>) => {
+  let index = 0
+
+  return new Transform({
+    objectMode: true,
+    transform: async (chunk, _, next) => {
+      try {
+        await callback(chunk, index)
+        index++
+        next(null, chunk)
+      } catch (err) {
+        next(err)
+      }
+    }
+  })
+}
+
+export const reduceStream = <A, C>(callback: TReduceCallback<A, C>, initialValue: A) => {
+  let index = 0
+
+  return new Transform({
+    objectMode: true,
+    transform: async (chunk, _, next) => {
+      try {
+        initialValue = await callback(initialValue || chunk, chunk, index)
+        index++
+        next()
+      } catch (err) {
+        next(err)
+      }
+    },
+    flush: (next) => {
+      next(null, initialValue)
+    }
+  })
+}
+
+export const mapStream = <T, R>(callback: TMapCallback<T, R>) => {
+  let index = 0
+  return new Transform({
+    objectMode: true,
+    transform: async (chunk, _, next) => {
+      try {
+        next(null, await callback(chunk, index))
+        index++
+      } catch (err) {
+        next(err)
+      }
+    }
+  })
+}
